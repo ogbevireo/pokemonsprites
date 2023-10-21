2fda6d2bb58d68e548df3863c323b61554c30cbf
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-10-21 19:37:36 +0100

    initial setup

---
+++ pokemonsprites.js
@@ -0,0 +1,8 @@
+//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+
+const container = document.querySelector('#container');
+const newImg = document.createElement('img');
+newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+
+container.appendChild(newImg)
